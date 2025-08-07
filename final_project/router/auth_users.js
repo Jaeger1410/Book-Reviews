62fb7f1b46756e05612df01c5cb05c1bda31f00f
Carlos Cortes <cortespoblete.carlos@gmail.com>
2025-08-07 12:00:24 -0400

    Upte auth_users.js, general.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,57 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    //write code to check is the username is valid
+    let validUser = users.filter((user) => {
+        return (user.username === username);
+    })
+
+    if (validUser) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+    //write code to check if username and password match the one we have in records.
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+
+    if (authenticatedUser(username)) {
+        let accessToken = jwt.sign({
+            data: username
+        }, 'access', { expiresIn: 60 * 60});
+
+        // Stores access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        };
+        return res.status(200).send("User succesfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }    
+
+
 });
 
 // Add a book review
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,21 +18,6 @@ const doesExist = (username) => {
     }
 }
 
-// Check if the user with the given username and password exists
-const authenticatedUser = (username, password) => {
-    // Filter the users array for any user with the same username and password
-    let validusers = users.filter((user) => {
-        return (user.username === username && user.password === password);
-    });
-    // Return true if any valid user is found, otherwise false
-    if (validusers.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
